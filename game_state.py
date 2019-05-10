80c555662c975865b4c6edb96ed6d691789630e0
SeongminHa <49818274+SeongminHa@users.noreply.github.com>
2019-05-10 19:39:22 +0900

    using game-world

--- game_state.py
+++ game_state.py
@@ -1,16 +1,20 @@
 from pico2d import *
 import random
+import game_framework
+import game_world
 
 class Grass:
     def __init__(self):
         self.image=load_image('grass.png')
     def draw(self):
         self.image.draw(400, 30)
+    def update(self):
+        pass
 
 class Boy:
     def __init__(self):
         self.image=load_image('animation_sheet.png')
-        self.frame == 0
+        self.frame = 0
         self.x = random.randint(100,700)
         self.y = random.randint(100,500)
         self.dx, self.dy=0,0
@@ -22,51 +26,102 @@ class Boy:
         self.frame = (self.frame + 1) % 8
         self.x += self.dx
         self.y += self.dy
+    def fire_ball(self):
+        speed = -1 if self.dx < 0 else 1
+        ball = Ball(self.x, self.y, speed)
+        game_world.add_object(ball, game_world.layer_obstacle)
+
+class Ball:
+    image=None
+    def __init__(self, x, y, speed):
+        self.x, self.y= x, y
+        self.speed=speed
+        if Ball.image==None:
+            Ball.image=load_image('ball21x21.png')
+    def update(self):
+        self.x += self.speed
+    def draw(self):
+        Ball.image.draw(self.x, self.y)
 
 def handle_events():
-    global running, b
+    global boy
     events = get_events()
     for e in events:
         if e.type==SDL_QUIT:
-            running = False
+            # running = False
+            game_framework.quit()
         elif e.type == SDL_MOUSEMOTION:
-            b.x, b.y=e.x, get_canvas_height() - e.y
+            boy.x, boy.y=e.x, get_canvas_height() - e.y
         elif e.type == SDL_KEYDOWN:
             if e.key == SDLK_ESCAPE:
                 running = False
             elif e.key == SDLK_UP:
-                b.dy += 1
+                boy.dy += 1
             elif e.key == SDLK_DOWN:
-                b.dy -= 1
+                boy.dy -= 1
             elif e.key == SDLK_LEFT:
-                b.dx -= 1
+                boy.dx -= 1
             elif e.key == SDLK_RIGHT:
-                b.dx += 1
+                boy.dx += 1
+            elif e.key == SDLK_SPACE:
+                boy.fire_ball()
         elif e.type == SDL_KEYUP:
             if e.key == SDLK_UP:
-                b.dy -= 1
+                boy.dy -= 1
             elif e.key == SDLK_DOWN:
-                b.dy += 1
+                boy.dy += 1
             elif e.key == SDLK_LEFT:
-                b.dx += 1
+                boy.dx += 1
             elif e.key == SDLK_RIGHT:
-                b.dx -= 1
+                boy.dx -= 1
 
-open_canvas()
-g = Grass()
-b = Boy()
-boys=[Boy() for i in range(10)]
-running = True
-while (running):
+def enter():
+    grass=Grass()
+    game_world.add_object(grass, game_world.layer_bg)
+
+    global boy
+    boy=Boy()
+    game_world.add_object(boy, game_world.layer_player)
+
+
+def exit():
+    pass
+
+def update():
+    game_world.update()
+    # global boy
+    # boy.update()
+
+def draw():
+    # global grass, boy
+    # grass.draw()
+    # boy.draw()
     clear_canvas()
-    g.draw()
-    b.draw()
-    for boy in boys:
-        boy.draw()
+    game_world.draw()
     update_canvas()
-    delay(0.005)
-    handle_events()
-    b.update()
-    for boy in boys:
-        boy.update()
-close_canvas()
+
+
+if __name__ == '__main__' :
+    import sys
+    current_module=sys.modules[__name__]
+    open_canvas()
+    game_framework.run(current_module)
+    close_canvas()
+
+# # g = Grass()
+# b = Boy()
+# boys=[Boy() for i in range(10)]
+# running = True
+# while (running):
+#     clear_canvas()
+#     g.draw()
+#     b.draw()
+#     for boy in boys:
+#         boy.draw()
+#     update_canvas()
+#     delay(0.005)
+#     handle_events()
+#     b.update()
+#     for boy in boys:
+#         boy.update()
+# close_canvas()
--- logo_state.py
+++ logo_state.py
@@ -13,14 +13,14 @@ def exit():
     del(image)
     pass
 
-def update():
+def update(): # 흐른 시간을 계속 더함
     global time
     time += game_framework.frame_time # 이전 업데이트가 불린 시점부터 지금 업데이트가 불리는 시점까지의 시간
     if (time>1.0): # 누적시간 1초가 지나면
         game_framework.change_state(title_state) # exit 함수 호출
     delay(0.1)
 
-def draw():
+def draw(): # 계속 화면 그림
     global image
     # print('draw', image.w, image.h)
     image.draw(400, 300)
