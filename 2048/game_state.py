68571bc42f9f6d653ea836fecde70c74d955ae6f
SeongminHa <49818274+SeongminHa@users.noreply.github.com>
2019-05-29 01:55:13 +0900

    Update game_state.py
    Create Block class ready for 16 blocks

--- 2048/game_state.py
+++ 2048/game_state.py
@@ -3,6 +3,18 @@ from pico2d import *
 import game_framework
 import font
 
+# 2048의 블럭을 만드는 클래스
+class Block:
+    # 생성자
+    def __init__(self, x, y):
+        self.value=0
+        self.x, self.y = x,y
+        self.font = font.load('verdanab.ttf', 50)
+    def draw(self):
+        color = (0,0,0)
+        if self.value > 0:
+            self.font.draw(self.x, self.y, '%d' % self.value, color)
+
 def enter():
     pass
 
